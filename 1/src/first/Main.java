7c37fc35662abab46bee1873ea7b3eea29de0b04
el1191 <el1191@woyd.ru>
2019-01-15 18:16:44 +0300

    Add manifest

--- 1/src/first/Main.java
+++ 1/src/first/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("АиП 3.1, часть 1");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
